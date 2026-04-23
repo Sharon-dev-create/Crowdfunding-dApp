3fb66be7db40da3018508321c100a731135f7939
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-04-23 13:33:22 +0100

    some commit

--- frontend/components/Sidebar.jsx
+++ frontend/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { logo, sun } from "../src/assets";
 import { navlinks } from "../constants";
 
@@ -37,6 +37,27 @@ const Sidebar = () => {
       <Link to="/" className="flex items-center justify-center">
         <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
       </Link>
+
+      <div className="flex-1 flex flex-col
+      justify-between items-center bg-[#1c1c24]
+      rounded-[20px] w-[76px] py-4 mt-12">
+        <div className="flex flex-col justify-center
+        items-center gap-3">
+          {navlinks.map((link) => (
+            <Icon
+            key={link.name}
+            {...link}
+            isActive={isActive}
+             handleClick={() => {
+              if(!link.disabled) {
+                setIsActive(link.name);
+                Navigate(link.link)
+              }
+            }}/>
+           ))}
+        </div>
+
+      </div>
     </div>
   );
 };
