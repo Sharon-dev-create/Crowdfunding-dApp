5910cd998ec51a208119315c8563fda0ddb510fd
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-04-28 04:58:38 +0100

    some commit

--- frontend/context/index.jsx
+++ frontend/context/index.jsx
@@ -39,21 +39,20 @@ export const StateContextProvider = ({ children }) => {
             const data = await createCampaignWrite({
                 address: "0x429b3B235FA8e532ed033AE9f50e62b05413F3c7",
                 abi,
-                functionName: "createCampaign"
+                functionName: "createCampaign",
                 args: [
                     address, //owner
                     form.description, //description
                     ethers.parseEther(form.target || "0"),
                     Math.floor(new Date(form.deadline).getTime() / 1000),
-                    form.image
-                ]
-            })
-            console.log("contract call success", data)
+                    form.image,
+                ],
+            });
+            console.log("contract call success", data);
+        } catch (error) {
+            console.log("contract call failure", error);
         }
-        catch (error) {
-            console.log("contract call failure", error)
-        }
-    }
+    };
 
     return (
         <StateContext.Provider
@@ -63,12 +62,12 @@ export const StateContextProvider = ({ children }) => {
                 isConnecting,
                 connect,
                 disconnect,
-                createCampaign: publishCampaign
+                createCampaign: publishCampaign,
             }}
-            >
+        >
             {children}
-            </StateContext.Provider>
-    )
-}
+        </StateContext.Provider>
+    );
+};
 
-export const useStateContext = () => useContext(StateContext);  
+export const useStateContext = () => useContext(StateContext);
--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [campaigns, setCampaigns] = useState([]);
     return (
         <div>Home</div>
     )
